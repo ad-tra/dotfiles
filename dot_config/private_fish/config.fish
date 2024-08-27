bd1d68110070a7df4c3ab2067bfdf2e678392ee9
Adam Trabelsi <aew31gcmuy@gmail.com>
2024-08-27 14:46:40 -0400

    aerospace

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,17 +1,18 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    alias hx="helix"
     alias lb="lockbook"
     alias c="clear"
     alias x="exit"
     # Clipboard managment for wayland
-    alias pbcopy="wl-copy"
-    alias pbpaste="wl-paste"
-    alias gen_pswd="tr -dc A-Za-z0-9 </dev/urandom | head -c 13 | pbcopy"
+    # alias pbcopy="wl-copy"
+    # alias pbpaste="wl-paste"
     # Git
     alias gm='git commit -m'
     alias gp="git push"
-
+    alias gc='git checkout'
+    alias ga='git add'
+    
     bind -k btab forward-char
+    set -x DOCKER_HOST unix:///Users/adam/.colima/default/docker.sock
 end
-
+set -x USE_GKE_GCLOUD_AUTH_PLUGIN True
